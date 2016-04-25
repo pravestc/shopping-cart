a6fdbf885aa576f90c228e419f6f35d3dda86451
pravestc <pravest@gmail.com>
2016-04-25 18:57:53 -0500

    Added a Shopkeeper's page that is restricted for non-shopkeepers users

--- app/controllers/shopkeeper/application_controller.rb
+++ app/controllers/shopkeeper/application_controller.rb
@@ -1,4 +1,16 @@
 class Shopkeeper::ApplicationController < ApplicationController
+  before_action :authorize_shopkeeper!
+
   def index
+  end
+
+  private
+
+  def authorize_shopkeeper!
+    authenticate_user!
+
+    unless current_user.shopkeeper?
+      redirect_to root_path, alert: "You must be a shopkeeper to do that."
+    end
   end
 end
--- spec/controllers/shopkeeper/application_controller_spec.rb
+++ spec/controllers/shopkeeper/application_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Shopkeeper::ApplicationController, type: :controller do
+  let(:user) { FactoryGirl.create(:user) }
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
+  before do
+    allow(controller).to receive(:authenticate_user!)
+    allow(controller).to receive(:current_user).and_return(user)
   end
 
+  context "non-shopkeepers" do
+    it "are not able to access the index action" do
+      get :index
+
+      expect(response).to redirect_to "/"
+      expect(flash[:alert]).to eq "You must be a shopkeeper to do that."
+    end
+  end
 end
