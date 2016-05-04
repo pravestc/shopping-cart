04aa2718f6071acda9d3a93913750d6bc4196219
pravestc <pravest@gmail.com>
2016-05-04 18:52:22 -0500

    Removed old migrations

--- db/migrate/20160427204225_remove_tota_pricefrom_cart.rb
+++
@@ -1,4 +0,0 @@
-class RemoveTotaPricefromCart < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20160427204533_remove_totaprice_from_carts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTotapriceFromCarts < ActiveRecord::Migration
-  def change
-    remove_column :carts, :totaprice, :integer
-  end
-end
